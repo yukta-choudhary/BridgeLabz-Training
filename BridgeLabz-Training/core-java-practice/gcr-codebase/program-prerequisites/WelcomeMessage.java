d3d1f86deca4e317ed89e807935f86b7f4cffbc9
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-17 00:47:20 +0530

    [Yukta]: added Welcome Message

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/program-prerequisites/WelcomeMessage.java
@@ -0,0 +1,6 @@
+// This program prints a welcome message
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
