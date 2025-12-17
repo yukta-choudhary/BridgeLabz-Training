c8f100859bd29806c2717f8001bf50742ee03e3c
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-17 12:00:29 +0530

    [Yukta]: added Calculate age of Harry

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/programming-elements/level1/CalculateAge.java
@@ -0,0 +1,14 @@
+// This program calculates age of Harry when current year is 2024.
+public class CalculateAge{
+    public static void main(String[] args){
+
+        // Create a variable ageHarry, birthYearHarry and currentYear=2024
+        int birthYearHarry = 2000, currentYear = 2024, ageHarry;
+
+        // Calculate age of Harry
+        ageHarry = currentYear - birthYearHarry;
+
+        // Print the age of Harry
+        System.out.println("Age of Harry is " + ageHarry);
+    }
+}
