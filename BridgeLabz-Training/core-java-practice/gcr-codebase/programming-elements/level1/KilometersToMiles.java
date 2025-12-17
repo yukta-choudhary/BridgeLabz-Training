e0316384c90744c6c80cde2583b74092db39efc2
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-17 14:53:36 +0530

    [Yukta]: added Kilometers to miles

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/programming-elements/level1/KilometersToMiles.java
@@ -0,0 +1,16 @@
+// This program converts kilometers to miles
+
+public class KilometersToMiles{
+    public static void main(String[] args){
+
+        // Create variable kilometers and miles
+        double kilometers = 10.8;
+        double miles;
+
+        // Calculate miles
+        miles = kilometers * 1.6;
+
+        // Print miles
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
