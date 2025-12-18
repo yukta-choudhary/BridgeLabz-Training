77b81fc5568ba8b18c404bd285142c82c8d9ceeb
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-18 17:16:37 +0530

    [Yukta]: added  CompareComputationsFor

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/control-flow/level1/CompareComputationsUsingFor.java
@@ -0,0 +1,33 @@
+// Find the sum of n natural numbers using for loop compare the result with the formulae n*(n+1)/2 and show the result from both computations was correct. 
+
+import java.util.Scanner;
+
+public class CompareComputationsUsingFor{
+    public static void main(String[] args){
+        
+       //Create object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Create variable sumWhile, number, sumFormulae
+		int number, sumWhile = 0, sumFormulae = 0;
+        System.out.print("Enter number: ");
+		number = input.nextInt();
+		
+        // Check if number is a natural number. Is yes then calculate sum using for and formulae.
+        if(number>=0){
+			sumFormulae = (number*(number+1))/2;
+			
+			for(int i = number; i>=1; i--){
+					sumWhile = sumWhile + i;
+			}			
+			System.out.println("The sum from while loop and formulae is same? " + (sumWhile == sumFormulae) + ". The sum is " + sumWhile);
+			
+		}else{
+			System.out.println("The number is " + number + "not a Natural Number");
+		}
+
+        //Close Scanner object
+        input.close();
+
+    }
+}
