604bf8135851b8e27d4ac3621e29f0aac33356b3
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-18 22:07:25 +0530

    [Yukta]: added CalculateFactorialUsingFor

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/control-flow/level1/CalculateFactorialUsingFor.java
@@ -0,0 +1,30 @@
+// Find the factorial of an integer entered by the user using for loop
+import java.util.Scanner;
+
+public class CalculateFactorialUsingFor{
+    public static void main(String[] args){
+        
+       //Create object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Create variable number,factorial
+		int number, factorial = 1;
+        System.out.print("Enter number: ");
+		number = input.nextInt();
+		
+        // Check if number is positive. If yes then calculate its factorial
+        if(number>=0){
+			for(int i = number; i>=1; i--){
+					factorial = factorial * i;
+			}			
+			System.out.println("The factorial of the number " + number + " is " + factorial);
+			
+		}else{
+			System.out.println("The number is " + number + " not a positive number");
+		}
+
+        //Close Scanner object
+        input.close();
+
+    }
+}
