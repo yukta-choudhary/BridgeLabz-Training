ffc7b2d6d0c631841e73688105eebb8ad397d517
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-18 15:33:37 +0530

    [Yukta]: added RocketLaunchCounter

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/control-flow/level1/RocketLaunchCounter.java
@@ -0,0 +1,26 @@
+// Create a counter for rocket launch from user input value to 1.
+
+import java.util.Scanner;
+
+public class RocketLaunchCounter{
+    public static void main(String[] args){
+        
+       //Create object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Create variable counter
+		int counter;
+        System.out.print("Enter counter value:  ");
+        counter = input.nextInt();
+		
+        // Run a loop for countdown
+        while(counter>=1){
+			System.out.println(counter + "\n");
+			counter = counter - 1;
+		}
+        
+        //Close Scanner object
+        input.close();
+
+    }
+}
