6f6d102d326c53697705167a9f0789b93c46cc6f
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-18 11:58:39 +0530

    [Yukta]: added CalculateHeight

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/programming-elements/level1/CalculateHeight.java
@@ -0,0 +1,24 @@
+// This program calculates height from centimeters to feet and inches 
+
+import java.util.Scanner;
+
+public class CalculateHeight{
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+        // Create variable heightCm, heightInch and heightFeet
+        int heightCm;
+        double heightFeet, heightInch;
+        System.out.println("Enter height in Cm: ");
+        heightCm = input.nextInt();
+
+        // Calculate height in feet and inches
+        
+	heightInch = (double)heightCm/2.54;
+        heightFeet = heightInch/(double)12;
+
+        // Print height in feet and inches
+        System.out.println(" Your Height in cm is " + heightCm + " while in feet is " + heightFeet + " and inches is " + heightInch);
+    }
+}
