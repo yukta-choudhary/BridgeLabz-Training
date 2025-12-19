57905fb4843c8fafe6f684afb40de4c526b090ed
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-19 15:00:56 +0530

    [Yukta]: added DayOfWeek

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/control-flow/level3/DayOfWeek.java
@@ -0,0 +1,19 @@
+public class DayOfWeek {
+    public static void main(String[] args) {
+
+        // Take command Line inputs
+        int m = Integer.parseInt(args[0]); // month
+        int d = Integer.parseInt(args[1]); // day
+        int y = Integer.parseInt(args[2]); // year
+
+        // Perform operations by applying formula
+        int y0 = y - (14 - m) / 12;
+        int x  = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+		
+        // Print the result
+        System.out.println(d0);
+		input.close();
+	}
+}
