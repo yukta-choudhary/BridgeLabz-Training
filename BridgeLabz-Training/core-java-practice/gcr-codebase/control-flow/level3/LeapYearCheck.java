e63c9e5a5aa70c9d257d6ef398dfe9028d48625b
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-19 12:27:16 +0530

    [Yukta]: modified LeapYearCheck

--- BridgeLabz-Training/core-java-practice/gcr-codebase/control-flow/level3/LeapYearCheck.java
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/control-flow/level3/LeapYearCheck.java
@@ -14,18 +14,20 @@ public class LeapYearCheck {
 
         // Check for leap year and display message
 		if(year >= 1582){
-			if(year % 4 == 0){
-				if (year%400 == 0){
-					System.out.println("The year " + year + " is a leap year");
-				}else{
+			if(year % 400 == 0){
+				System.out.println("The year " + year + " is a leap year");
+			
+			}else if (year%100 == 0){
 					System.out.println("The year " + year + " is not a leap year");
-				}
+			}
+			else if(year%4 ==0){
+				System.out.println("The year " + year + " is a leap year");
 			}else{
-			System.out.println("The year " + year + " is not a leap year");
+				System.out.println("The year " + year + " is not a leap year");
 			}
 		}
 		else{
-			System.out.println("The year " + year + " is not a leap year");
+			System.out.println("The year " + year + " must be greater than or equal to 1582.");
 		}
 
         // Close scanner
