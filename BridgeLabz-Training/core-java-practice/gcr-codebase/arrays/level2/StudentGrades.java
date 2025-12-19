81fd938162ffe1e74c4a2f0f424db5f036638738
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-19 23:35:51 +0530

    [Yukta]: added StudentGrades

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/arrays/level2/StudentGrades.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class StudentGrades
+{
+	public static void main(String[] args)
+	{
+		// Create scanner class object
+		Scanner input = new Scanner(System.in);
+		
+		// Declare number of students
+		System.out.println("Enter number of students:");
+		int number = input.nextInt();
+		
+		// Declare 2D array for marks and arrays for percentage and grade
+		double[][] marks = new double[number][3];
+		double[] percentage = new double[number];
+		String[] grade = new String[number];
+		
+		// Take input for marks
+		for(int i = 0; i < number; i++){
+			System.out.println("Enter marks in Physics for student " + (i + 1) + ":");
+			marks[i][0] = input.nextDouble();
+			if(marks[i][0] < 0){
+				System.out.println("Invalid marks, enter again");
+				i--;
+				continue;
+			}
+			
+			System.out.println("Enter marks in Chemistry for student " + (i + 1) + ":");
+			marks[i][1] = input.nextDouble();
+			if(marks[i][1] < 0){
+				System.out.println("Invalid marks, enter again");
+				i--;
+				continue;
+			}
+			
+			System.out.println("Enter marks in Maths for student " + (i + 1) + ":");
+			marks[i][2] = input.nextDouble();
+			if(marks[i][2] < 0){
+				System.out.println("Invalid marks, enter again");
+				i--;
+			}
+		}
+		
+		// Calculate percentage and grade
+		for(int i = 0; i < number; i++){
+			percentage[i] = (marks[i][0] + marks[i][1] + marks[i][2]) / 3;
+			if(percentage[i] >= 90){
+				grade[i] = "A";
+			}else if(percentage[i] >= 75){
+				grade[i] = "B";
+			}else if(percentage[i] >= 50){
+				grade[i] = "C";
+			}else{
+				grade[i] = "F";
+			}
+		}
+		
+		// Display marks, percentage and grade
+		System.out.println("Physics\tChemistry\tMaths\tPercentage\tGrade");
+		for(int i = 0; i < number; i++){
+			System.out.printf("%.2f\t%.2f\t\t%.2f\t%.2f\t\t%s%n",
+					marks[i][0], marks[i][1], marks[i][2], percentage[i], grade[i]);
+		}
+		
+		// Close Scanner
+		input.close();
+	}
+}
