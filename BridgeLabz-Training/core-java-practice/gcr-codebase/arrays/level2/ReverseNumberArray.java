f8982e002d0574a36d2bd420330fa0a0f3e5ea88
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-22 00:06:44 +0530

    [Yukta]: modified logic in ReverseNumberArray

--- BridgeLabz-Training/core-java-practice/gcr-codebase/arrays/level2/ReverseNumberArray.java
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/arrays/level2/ReverseNumberArray.java
@@ -2,45 +2,47 @@ import java.util.Scanner;
 
 public class ReverseNumberArray
 {
-	public static void main(String[] args)
-	{
-		// Create scanner class object
-		Scanner input = new Scanner(System.in);
-		
-		// Declare number variable
-		System.out.println("Enter a number:");
-		int number = input.nextInt();
-		
-		int temp = number;
-		int count = 0;
-		
-		// Find count of digits
-		while(temp != 0){
-			count++;
-			temp = temp / 10;
-		}
-		
-		// Declare array to store digits
-		int[] digits = new int[count];
-		
-		for(int i = 0; i < count; i++){
-			digits[i] = number % 10;
-			number = number / 10;
-		}
-		
-		int[] reverseArray = new int[count];
-		
-		for(int i = 0; i < count; i++){
-			reverseArray[i] = digits[count - 1 - i];
-		}
-		
-		// Display reversed number
-		System.out.println("Reversed number is:");
-		for(int i = 0; i < count; i++){
-			System.out.print(reverseArray[i]);
-		}
-		
-		// Close Scanner class object
-		input.close();
-	}
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int temp = number;
+        int count = 0;
+
+        // Count digits
+        while (temp > 0)
+        {
+            count++;
+            temp = temp / 10;
+        }
+
+        int[] digits = new int[count];
+        temp = number;
+
+        // Store digits in array
+        for (int i = count - 1; i >= 0; i--)
+        {
+            digits[i] = temp % 10;
+            temp = temp / 10;
+        }
+
+        int[] reverseArray = new int[count];
+
+        // Reverse the array
+        for (int i = 0; i < count; i++)
+        {
+            reverseArray[i] = digits[count - 1 - i];
+        }
+
+        System.out.println("Reversed number is:");
+        for (int i = 0; i < count; i++)
+        {
+            System.out.print(reverseArray[i]);
+        }
+
+        input.close();
+    }
 }
