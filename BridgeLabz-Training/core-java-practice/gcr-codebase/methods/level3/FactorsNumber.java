30a5b0f5c845a83d5354b58abf19f90933649226
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-22 01:03:20 +0530

    [Yukta]: added FactorsNumber

---
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/methods/level3/FactorsNumber.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class FactorsNumber {
+
+    // Count factors of a number
+    public static int countFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Find factors and return as array
+    public static int[] findFactors(int number) {
+        int count = countFactors(number);
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index] = i;
+                index++;
+            }
+        }
+        return factors;
+    }
+
+    // Find greatest factor
+    public static int greatestFactor(int[] factors) {
+        return factors[factors.length - 1]; 
+    }
+
+    // Find sum of factors
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int i = 0; i < factors.length; i++) {
+            sum += factors[i];
+        }
+        return sum;
+    }
+
+    // Find product of factors
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int i = 0; i < factors.length; i++) {
+            product *= factors[i];
+        }
+        return product;
+    }
+
+    // Find product of cube of factors
+    public static long productOfCubes(int[] factors) {
+        long product = 1;
+        for (int i = 0; i < factors.length; i++) {
+            product *= Math.pow(factors[i], 3);
+        }
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take number input
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Find factors
+        int[] factors = findFactors(number);
+
+        // Display factors
+        System.out.print("Factors: ");
+        for (int i = 0; i < factors.length; i++) {
+            System.out.print(factors[i] + " ");
+        }
+        System.out.println();
+
+        // Display results
+        System.out.println("Greatest Factor: " + greatestFactor(factors));
+        System.out.println("Sum of Factors: " + sumOfFactors(factors));
+        System.out.println("Product of Factors: " + productOfFactors(factors));
+        System.out.println("Product of Cubes of Factors: " + productOfCubes(factors));
+    }
+}
