20036f1204928c714e5757fc3b1b126693bb005c
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2025-12-23 12:24:09 +0530

    [Yukta]: modified SubStringCompare.java

--- BridgeLabz-Training/core-java-practice/gcr-codebase/strings/level1/SubStringCompare.java
+++ BridgeLabz-Training/core-java-practice/gcr-codebase/strings/level1/SubStringCompare.java
@@ -4,13 +4,29 @@ public class SubStringCompare {
 
 	//Create a method to create substring using charAt method
 	public static String SubStringUsingCharAt(String str, int startIndex, int endIndex){
+		if(startIndex>=0 && endIndex<str.length() && startIndex<endIndex){
+			System.out.println("Index is valid");
+		}
 		String result = "";
 		for(int i = startIndex; i<endIndex; i++){
 			result = result + str.charAt(i);
 		}
 		return result;
 	}
-
+	
+	//Create a method to compare two strings using charAt
+	public static boolean compareUsingCharAt(String string1, String string2){
+		if(string1.length() != string2.length()){
+			return false;
+		}
+		for(int i=0; i<string1.length(); i++){
+			if(string1.charAt(i) != string2.charAt(i)){
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args){
 	
 		Scanner input = new Scanner(System.in);
@@ -31,8 +47,11 @@ public class SubStringCompare {
 		//Create substring using substring() method
 		String subStringResult = str.substring(startIndex, endIndex);
 		
+		//Compare both strings using charAt and display result accordingly
+		boolean compareResult = compareUsingCharAt(subStringResult, charAtResult);
+		
 		//Check if both results are equal
-		if(charAtResult.equals(subStringResult)){
+		if(compareResult){
 			System.out.println("Comparison using charAt and substring method, the result is same.");
 		}else{
 			System.out.println("Comparison using charAt and substring method, the result is not same.");
