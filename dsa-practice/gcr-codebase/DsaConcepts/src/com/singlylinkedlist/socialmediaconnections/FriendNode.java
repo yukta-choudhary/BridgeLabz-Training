4d358cc38bbf487b52f5e8c6486314fe92038e11
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-02 22:50:32 +0530

    [Yukta]: added FriendNode

---
+++ dsa-practice/gcr-codebase/DsaConcepts/src/com/singlylinkedlist/socialmediaconnections/FriendNode.java
@@ -0,0 +1,13 @@
+package com.singlylinkedlist.socialmediaconnections;
+
+//Node for Friend ID list
+
+class FriendNode {
+ int friendId;
+ FriendNode next;
+
+ FriendNode(int friendId) {
+     this.friendId = friendId;
+     this.next = null;
+ }
+}
