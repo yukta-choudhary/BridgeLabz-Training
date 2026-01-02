32b85546f50e6f6454950a2c202c522d3bd52808
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-02 16:37:55 +0530

    [Yukta]: added Enrollment

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day2/universitycourseenrollment/Enrollment.java
@@ -0,0 +1,9 @@
+package com.day2.universitycourseenrollment;
+
+//Enrollment
+public class Enrollment {
+
+ public void enroll(Student student, Course course) {
+     System.out.println(student.name + " enrolled in " + course);
+ }
+}
