c6cd3ca5a005e1ac1c616b8ff2ee0e97d5856d58
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-02 23:01:12 +0530

    [Yukta]: added MovieNode

---
+++ dsa-practice/gcr-codebase/DsaConcepts/src/com/singlylinkedlist/moviemanagementsystem/MovieNode.java
@@ -0,0 +1,22 @@
+package com.singlylinkedlist.moviemanagementsystem;
+//Node class for Doubly Linked List
+
+class MovieNode {
+ String title;
+ String director;
+ int year;
+ double rating;
+
+ MovieNode next;
+ MovieNode prev;
+
+ // Constructor
+ MovieNode(String title, String director, int year, double rating) {
+     this.title = title;
+     this.director = director;
+     this.year = year;
+     this.rating = rating;
+     this.next = null;
+     this.prev = null;
+ }
+}
