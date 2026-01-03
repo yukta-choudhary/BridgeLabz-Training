ab38bec0d6e97a3026e3dc1f83b29054d1ae3c59
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-03 15:07:08 +0530

    [Yukta]: modified BankMain

--- java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day1/bankmanagementsystem/BankMain.java
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day1/bankmanagementsystem/BankMain.java
@@ -9,11 +9,11 @@ public class BankMain {
      SavingsAccount sa = new SavingsAccount(101, 5000, 5);
 
      // Create customer
-     Customer c1 = new Customer("Rahul", sa);
+     Customer c1 = new Customer("John", sa);
 
      // Operations
      c1.showCustomerDetails();
-     sa.deposit(2000);
+     sa.deposit(5000);
      sa.withdraw(1000);
      sa.calculateInterest();
      sa.showBalance();
