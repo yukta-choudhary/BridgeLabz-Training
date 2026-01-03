97ac978a5616cabed79c485dfd900c6919b38958
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-03 19:26:08 +0530

    [Yukta]: added PetrolPump

---
+++ dsa-practice/gcr-codebase/DsaConcepts/src/com/stacksandqueues/circulartourproblem/PetrolPump.java
@@ -0,0 +1,14 @@
+package com.stacksandqueues.circulartourproblem;
+
+//Class to store petrol pump details
+public class PetrolPump {
+
+ int petrol;   // petrol available
+ int distance; // distance to next pump
+
+ // Constructor
+ public PetrolPump(int p, int d) {
+     petrol = p;
+     distance = d;
+ }
+}
