7d6af801ef0ddd7a55b087ee61fe6a27d55b7fbd
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-05 16:54:02 +0530

    [Yukta]: added Faculty

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day3/campusconnectprogram/Faculty.java
@@ -0,0 +1,22 @@
+package com.day3.campusconnectprogram;
+
+//Faculty class
+public class Faculty extends Person {
+
+ private String department;
+
+ // Constructor
+ public Faculty(int id, String name, String email, String department) {
+     super(id, name, email);
+     this.department = department;
+ }
+
+ // Polymorphism
+ @Override
+ public void printDetails() {
+     System.out.println("Faculty ID: " + id);
+     System.out.println("Name: " + name);
+     System.out.println("Email: " + email);
+     System.out.println("Department: " + department);
+ }
+}
