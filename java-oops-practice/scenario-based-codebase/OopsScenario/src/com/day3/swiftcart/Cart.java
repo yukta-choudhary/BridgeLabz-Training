9690ad7fd431780901b9a85bdfb54993909ea1f6
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-05 21:12:49 +0530

    [Yukta]: added Cart

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day3/swiftcart/Cart.java
@@ -0,0 +1,49 @@
+package com.day3.swiftcart;
+
+import java.util.*;
+
+//Cart class
+public class Cart implements ICheckout {
+
+ private List<Product> products;     // protected list
+ private double totalPrice;           // private price
+
+ // Default constructor
+ public Cart() {
+     products = new ArrayList<>();
+     totalPrice = 0;
+ }
+
+ // Constructor with pre-selected items
+ public Cart(List<Product> products) {
+     this.products = products;
+     calculateTotal();
+ }
+
+ // Only Cart can update price
+ private void calculateTotal() {
+     totalPrice = 0;
+
+     for (Product p : products) {
+         totalPrice = totalPrice + p.price; // + operator
+     }
+ }
+
+ // Add product
+ public void addProduct(Product p) {
+     products.add(p);
+     calculateTotal();
+ }
+
+ // Apply discount using polymorphism
+ public void applyDiscount() {
+     for (Product p : products) {
+         totalPrice = p.applyDiscount(totalPrice);
+     }
+ }
+
+ // Generate bill
+ public void generateBill() {
+     System.out.println("Total Bill Amount: " + totalPrice);
+ }
+}
