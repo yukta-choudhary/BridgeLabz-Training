88cd3896c9a181497db1c4b699e62c1f82421435
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-05 16:53:41 +0530

    [Yukta]: added ICourseActions

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day3/campusconnectprogram/ICourseActions.java
@@ -0,0 +1,9 @@
+package com.day3.campusconnectprogram;
+
+//Interface for course actions
+public interface ICourseActions {
+
+ void enrollCourse(String courseName);
+
+ void dropCourse(String courseName);
+}
