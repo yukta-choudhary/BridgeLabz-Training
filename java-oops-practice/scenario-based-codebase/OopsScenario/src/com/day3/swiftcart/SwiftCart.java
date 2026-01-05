a648cb0c768aeb3ec4637e482db2db2e5cb76a5d
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-05 21:14:30 +0530

    [Yukta]: added SwiftCart

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day3/swiftcart/SwiftCart.java
@@ -0,0 +1,57 @@
+package com.day3.swiftcart;
+
+/*
+Question:
+SwiftCart – The Grocery Shopping App
+*/
+
+import java.util.*;
+
+public class SwiftCart {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Cart cart = new Cart();
+
+        System.out.println("Enter number of products:");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Enter product name:");
+            String name = sc.nextLine();
+
+            System.out.println("Enter price:");
+            double price = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.println("Enter category:");
+            String category = sc.nextLine();
+
+            System.out.println("1. Perishable  2. Non-Perishable");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            Product p;
+
+            if (choice == 1) {
+                p = new PerishableProduct(name, price, category);
+            } else {
+                p = new NonPerishableProduct(name, price, category);
+            }
+
+            cart.addProduct(p);
+        }
+
+        // Apply discount
+        cart.applyDiscount();
+
+        // Generate bill
+        cart.generateBill();
+
+        sc.close();
+    }
+}
