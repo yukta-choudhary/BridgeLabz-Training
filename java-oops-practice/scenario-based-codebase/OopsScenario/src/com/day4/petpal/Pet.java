7d3c58c2d9433ec0d899a4c1a5205164198519c9
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:22:45 +0530

    [Yukta]: added Pet

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/petpal/Pet.java
@@ -0,0 +1,48 @@
+package com.day4.petpal;
+
+//Base Pet class
+public class Pet {
+
+ String name;
+ String type;
+ int age;
+
+ private int hunger; // encapsulated
+ private int mood;   // encapsulated
+
+ // Constructor with random values
+ Pet(String name, String type, int age) {
+     this.name = name;
+     this.type = type;
+     this.age = age;
+     hunger = 5;
+     mood = 5;
+ }
+
+ // Feed pet
+ void reduceHunger() {
+     hunger--;
+ }
+
+ // Improve mood
+ void improveMood() {
+     mood++;
+ }
+
+ // Sleep increases mood
+ void rest() {
+     mood += 2;
+ }
+
+ // Display status
+ void showStatus() {
+     System.out.println("Hunger Level: " + hunger);
+     System.out.println("Mood Level: " + mood);
+ }
+
+ // Polymorphism
+ void makeSound() {
+     System.out.println("Pet makes sound");
+ }
+}
+
