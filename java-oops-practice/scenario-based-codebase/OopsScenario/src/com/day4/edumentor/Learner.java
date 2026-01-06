a21e94097a4a86eb05251032f1a3a4c53ee2a1ad
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:09:32 +0530

    [Yukta]: added Learner

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/edumentor/Learner.java
@@ -0,0 +1,23 @@
+package com.day4.edumentor;
+
+//Learner class
+public class Learner extends User implements ICertifiable {
+
+ String courseType; // short or full-time
+
+ // Constructor
+ Learner(String name, String email, int userId, String courseType) {
+     super(name, email, userId);
+     this.courseType = courseType;
+ }
+
+ // Polymorphism
+ public void generateCertificate() {
+     if (courseType.equals("short")) {
+         System.out.println("Short Course Certificate Generated");
+     } else {
+         System.out.println("Full-Time Course Certificate Generated");
+     }
+ }
+}
+
