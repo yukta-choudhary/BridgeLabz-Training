57050dda00ee01aa2bdfebfc3b59c6b57a2cfe5c
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:12:03 +0530

    [Yukta]: added Event

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/eventease/Event.java
@@ -0,0 +1,39 @@
+package com.day4.eventease;
+
+//Base Event class
+public class Event {
+
+ String eventName;
+ String location;
+ String date;
+ int attendees;
+
+ private final int eventId; // cannot be changed
+ protected int venueCost;
+ protected int serviceCost;
+ protected int discount;
+
+ // Constructor without services
+ Event(int eventId, String eventName, String location, String date, int attendees) {
+     this.eventId = eventId;
+     this.eventName = eventName;
+     this.location = location;
+     this.date = date;
+     this.attendees = attendees;
+     venueCost = 5000;
+ }
+
+ // Constructor with services
+ Event(int eventId, String eventName, String location, String date, int attendees,
+       int serviceCost, int discount) {
+     this(eventId, eventName, location, date, attendees);
+     this.serviceCost = serviceCost;
+     this.discount = discount;
+ }
+
+ // Cost calculation
+ int calculateCost() {
+     return venueCost + serviceCost - discount;
+ }
+}
+
