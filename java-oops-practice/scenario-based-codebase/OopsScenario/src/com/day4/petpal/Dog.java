585a1c363f3e75122d73c900679df18b0f18c73f
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:22:09 +0530

    [Yukta]: added Dog

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/petpal/Dog.java
@@ -0,0 +1,28 @@
+package com.day4.petpal;
+
+//Dog class
+public class Dog extends Pet implements IInteractable {
+
+ Dog(String name, int age) {
+     super(name, "Dog", age);
+ }
+
+ public void feed() {
+     reduceHunger();
+     System.out.println("Dog is eating");
+ }
+
+ public void play() {
+     improveMood();
+     System.out.println("Dog is playing");
+ }
+
+ public void sleep() {
+     rest();
+     System.out.println("Dog is sleeping");
+ }
+
+ void makeSound() {
+     System.out.println("Dog says: Woof!");
+ }
+}
