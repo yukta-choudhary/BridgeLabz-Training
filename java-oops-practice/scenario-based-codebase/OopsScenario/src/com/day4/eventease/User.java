84d95bb58cf7eb3f41ad398b1140fca5f0a5f877
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:13:30 +0530

    [Yukta]: added User

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/eventease/User.java
@@ -0,0 +1,14 @@
+package com.day4.eventease;
+
+//Organizer user class
+public class User {
+
+ String name;
+ private String email; // sensitive data
+
+ // Constructor
+ User(String name, String email) {
+     this.name = name;
+     this.email = email;
+ }
+}
