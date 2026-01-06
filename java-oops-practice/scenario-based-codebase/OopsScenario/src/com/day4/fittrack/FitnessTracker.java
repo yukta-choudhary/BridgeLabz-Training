d1d8a89158bd3fcb5972facd1920e17e18f33c66
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:17:19 +0530

    [Yukta]: added FitnessTracker

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/fittrack/FitnessTracker.java
@@ -0,0 +1,21 @@
+package com.day4.fittrack;
+
+//Fitness tracker class
+public class FitnessTracker implements ITrackable {
+
+ private int dailyTarget = 500; // private log
+ int caloriesBurned;
+
+ public void startWorkout() {
+     System.out.println("Workout Started");
+ }
+
+ public void stopWorkout() {
+     System.out.println("Workout Stopped");
+ }
+
+ // Progress calculation
+ int getProgress() {
+     return dailyTarget - caloriesBurned;
+ }
+}
