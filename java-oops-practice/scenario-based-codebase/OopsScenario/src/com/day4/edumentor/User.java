9eae8d4931b2a4c8c79ddb6a6548db4400ab360e
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:10:13 +0530

    [Yukta]: added User

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/edumentor/User.java
@@ -0,0 +1,15 @@
+package com.day4.edumentor;
+
+//Base class User
+public class User {
+ String name;
+ String email;
+ int userId;
+
+ // Constructor
+ User(String name, String email, int userId) {
+     this.name = name;
+     this.email = email;
+     this.userId = userId;
+ }
+}
