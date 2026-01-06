b90ca5cceab894416420ca8b9f26cd17a58d6d83
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:14:45 +0530

    [Yukta]: added CardioWorkout

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/fittrack/CardioWorkout.java
@@ -0,0 +1,15 @@
+package com.day4.fittrack;
+
+//Cardio workout
+public class CardioWorkout extends Workout {
+
+ CardioWorkout(int duration) {
+     super("Cardio", duration);
+ }
+
+ // Polymorphism
+ int calculateCalories() {
+     caloriesBurned = duration * 8;
+     return caloriesBurned;
+ }
+}
