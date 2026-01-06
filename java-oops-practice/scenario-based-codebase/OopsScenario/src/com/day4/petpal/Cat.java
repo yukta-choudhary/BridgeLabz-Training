003653492440f60c7447808a7c0508c0e17ce242
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-06 23:21:55 +0530

    [Yukta]: added Cat

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day4/petpal/Cat.java
@@ -0,0 +1,29 @@
+package com.day4.petpal;
+
+//Cat class
+public class Cat extends Pet implements IInteractable {
+
+ Cat(String name, int age) {
+     super(name, "Cat", age);
+ }
+
+ public void feed() {
+     reduceHunger();
+     System.out.println("Cat is eating");
+ }
+
+ public void play() {
+     improveMood();
+     System.out.println("Cat is playing");
+ }
+
+ public void sleep() {
+     rest();
+     System.out.println("Cat is sleeping");
+ }
+
+ void makeSound() {
+     System.out.println("Cat says: Meow!");
+ }
+}
+
