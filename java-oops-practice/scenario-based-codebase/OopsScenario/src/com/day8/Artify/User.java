f66d01b5d76e39bf34ba120ce441bd2b3eac146d
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-09 17:06:04 +0530

    [Yukta]: added User

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day8/Artify/User.java
@@ -0,0 +1,14 @@
+package com.day8.Artify;
+
+//User class
+public class User
+{
+ String name;
+ double walletBalance;
+
+ public User(String name, double walletBalance)
+ {
+     this.name = name;
+     this.walletBalance = walletBalance;
+ }
+}
