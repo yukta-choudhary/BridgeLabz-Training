22b9232b9c0ba1180bfdb93297ff76f093f40794
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-09 16:51:08 +0530

    [Yukta]: added User

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day8/SkillForge/User.java
@@ -0,0 +1,14 @@
+package com.day8.SkillForge;
+
+//Parent class
+public class User
+{
+ String name;
+ String email;
+
+ public User(String name, String email)
+ {
+     this.name = name;
+     this.email = email;
+ }
+}
