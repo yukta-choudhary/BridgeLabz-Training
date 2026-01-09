43d3763644c2f0cb799549724cfada408b5a39c6
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-09 17:00:33 +0530

    [Yukta]: added NonVegItem

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day8/FoodLoop/NonVegItem.java
@@ -0,0 +1,10 @@
+package com.day8.FoodLoop;
+
+//Child class
+public class NonVegItem extends FoodItem
+{
+ public NonVegItem(String name, double price, int availability)
+ {
+     super(name, "Non-Veg", price, availability);
+ }
+}
