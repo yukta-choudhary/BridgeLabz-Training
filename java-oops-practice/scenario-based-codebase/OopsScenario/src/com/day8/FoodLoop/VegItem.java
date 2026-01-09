81f2babd955786f4f23f47e06f0e05f4602153fe
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-09 17:02:04 +0530

    [Yukta]: added VegItem

---
+++ java-oops-practice/scenario-based-codebase/OopsScenario/src/com/day8/FoodLoop/VegItem.java
@@ -0,0 +1,10 @@
+package com.day8.FoodLoop;
+
+//Child class
+public class VegItem extends FoodItem
+{
+ public VegItem(String name, double price, int availability)
+ {
+     super(name, "Veg", price, availability);
+ }
+}
