a32c8cdd8b30658aa445ea377cf54e0bc4726a83
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-12 15:47:02 +0530

    [Yukta]: added Customer

---
+++ dsa-practice/scenario-based/DsaScenario/src/com/day2/callcenter/Customer.java
@@ -0,0 +1,14 @@
+package com.day2.callcenter;
+
+//Customer class
+class Customer {
+
+ String name;
+ boolean vip;
+
+ Customer(String name, boolean vip) {
+     this.name = name;
+     this.vip = vip;
+ }
+}
+
