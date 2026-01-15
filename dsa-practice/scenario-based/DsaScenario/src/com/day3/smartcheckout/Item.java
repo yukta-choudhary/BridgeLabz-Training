e7966dda289b7e39a541a496a11cfa4797d366f7
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-15 12:04:11 +0530

    [Yukta]: added Item

---
+++ dsa-practice/scenario-based/DsaScenario/src/com/day3/smartcheckout/Item.java
@@ -0,0 +1,16 @@
+package com.day3.smartcheckout;
+
+
+
+public class Item {
+    String name;
+    int price;
+    int stock;
+
+    // Constructor
+    public Item(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
