001aa636724e0c4cd26f465cd945fdf0e04487a3
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-15 11:41:36 +0530

    [Yukta]: added Book

---
+++ dsa-practice/scenario-based/DsaScenario/src/com/day2/bookshelf/Book.java
@@ -0,0 +1,13 @@
+package com.day2.bookshelf;
+//Book class
+class Book {
+
+ String title;
+ String author;
+
+ Book(String title, String author) {
+     this.title = title;
+     this.author = author;
+ }
+}
+
