bad2c5464060f4b98131dc9f0fcb0b8d017491bc
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-15 11:45:46 +0530

    [Yukta]: added Node

---
+++ dsa-practice/scenario-based/DsaScenario/src/com/day2/trafficmanager/Node.java
@@ -0,0 +1,12 @@
+package com.day2.trafficmanager;
+//Node for circular linked list
+class Node {
+
+ String car;
+ Node next;
+
+ Node(String car) {
+     this.car = car;
+     this.next = null;
+ }
+}
