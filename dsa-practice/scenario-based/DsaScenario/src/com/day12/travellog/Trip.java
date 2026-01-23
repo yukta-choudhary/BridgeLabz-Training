f0e81d03c60d13c93a57e18ec2547008d3af217e
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-23 22:50:32 +0530

    [Yukta]: added travellog

---
+++ dsa-practice/scenario-based/DsaScenario/src/com/day12/travellog/Trip.java
@@ -0,0 +1,18 @@
+package com.day12.travellog;
+
+import java.io.Serializable;
+
+//Serializable Trip object
+class Trip implements Serializable {
+ String city;
+ String country;
+ int days;
+ String notes;
+
+ Trip(String city, String country, int days, String notes) {
+     this.city = city;
+     this.country = country;
+     this.days = days;
+     this.notes = notes;
+ }
+}
