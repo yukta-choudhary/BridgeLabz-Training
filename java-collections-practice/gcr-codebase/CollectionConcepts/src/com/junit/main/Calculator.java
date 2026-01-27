104d88b1cae9b39c61001aedd3c97a0ee933f0b1
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-27 14:54:06 +0530

    [Yukta]: added Calculator

---
+++ java-collections-practice/gcr-codebase/CollectionConcepts/src/com/junit/main/Calculator.java
@@ -0,0 +1,21 @@
+// Simple calculator class
+package com.junit.main;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b; // throws exception if b = 0
+    }
+}
