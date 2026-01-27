4fcd5f24b5a5523c1880b5ca9ad5aebc6349ab4e
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-01-27 14:56:47 +0530

    [Yukta]: added ListManager

---
+++ java-collections-practice/gcr-codebase/CollectionConcepts/src/com/junit/main/ListManager.java
@@ -0,0 +1,19 @@
+package com.junit.main;
+
+import java.util.List;
+
+public class ListManager {
+
+    public void addElement(List<Integer> list, int element) {
+        list.add(element);
+    }
+
+    public void removeElement(List<Integer> list, int element) {
+        list.remove(Integer.valueOf(element));
+    }
+
+    public int getSize(List<Integer> list) {
+        return list.size();
+    }
+}
+
